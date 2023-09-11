55cd3ac59280866636dae765d53596ae10986f0c
Jonathan Nguyen <jnguyen74@myseneca.ca>
2023-09-10 21:52:19 -0400

    my first git commit

---
+++ lab1/lab1d.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 ** 5)'''
+
+#print(10 + 5 * 2)
+#print((10 + 5) * 2)
+#print(10 + 5 * 2 - 10 ** 2)
+#print(15 / 3 * 4)
+#print(100 / ((5 + 5) * 2))
+
+x = 10
+y = 2
+z = 5
+
+#print(str(x) + ' + ' str(y) + ' * ' str(z) + ' = ' + str(sum))
+print(str(x) + ' + ' +  str(y) + ' * ' +  str(z) + ' = ' + str(x + y * z))
